b007ac3a7e99c7ce4d297fe3fcb53ff7920bcd4f
Monir Nasserie <58013715+MN-Coding@users.noreply.github.com>
2023-04-27 01:00:40 -0400

    Create twosum.cpp

---
+++ Sequences/twosum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> pairs;
+        for (int i = 0; i < nums.size(); i++) {
+            if (pairs.find(nums[i]) != pairs.end()) {
+                return {i, pairs[nums[i]]};
+            }
+            pairs[target - nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+};
